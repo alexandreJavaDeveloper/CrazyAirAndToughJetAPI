26c0ee1747fbb9c6d71832f98d8696907b1c9de0
Alexandre <alexandreply@hotmail.com>
2017-02-01 23:18:02 -0200

    Modified the code structured and tests

--- src/main/java/com/suppliers/service/SuppliersService.java
+++ src/main/java/com/suppliers/service/SuppliersService.java
@@ -2,13 +2,15 @@ package com.suppliers.service;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.suppliers.mock.FactoryMock;
+import com.suppliers.model.CrazyAirFlightRequest;
 import com.suppliers.model.CrazyAirFlightResponse;
+import com.suppliers.model.ToughJetFlightRequest;
 import com.suppliers.model.ToughJetFlightResponse;
 
 @RestController
@@ -21,19 +23,33 @@ public class SuppliersService
 		this.factoryMock = new FactoryMock();
 	}
 
-	@RequestMapping(value = "/crazyair/{request}", method = RequestMethod.GET)
-	public List<CrazyAirFlightResponse> listCrazyAir(@PathVariable(value = "request")
-	final String requestJson)
+	/**
+	 * The request comes via JSON (default), but the Spring transforms in the java object declared.
+	 * This works because in the model (e.g: CrazyAirFlightRequest) we use the same attributes
+	 * and Jackson API can easily convert it.
+	 *
+	 * @param requestJson
+	 * @return
+	 */
+	@RequestMapping(value = "/crazyair", method = RequestMethod.POST)
+	public List<CrazyAirFlightResponse> listCrazyAir(@RequestBody final CrazyAirFlightRequest requestJson)
 	{
-		System.out.println("->"+requestJson);
+		System.out.println("->" + requestJson.getDepartureDate());
 		return this.factoryMock.getCrazyAirFlights();
 	}
 
-	@RequestMapping(value = "/toughjet/{request}", method = RequestMethod.GET)
-	public List<ToughJetFlightResponse> listToughJet(@PathVariable(value = "request")
-	final String requestJson)
+	/**
+	 * The request comes via JSON, but the Spring transforms in the java object declared.
+	 * This works because in the model (e.g: ToughJetFlightRequest) we use the same attributes
+	 * and Jackson API can easily convert it.
+	 *
+	 * @param requestJson
+	 * @return
+	 */
+	@RequestMapping(value = "/toughjet", method = RequestMethod.POST)
+	public List<ToughJetFlightResponse> listToughJet(@RequestBody final ToughJetFlightRequest requestJson)
 	{
-		System.out.println("->"+requestJson);
+		System.out.println("->" + requestJson.getFrom());
 		return this.factoryMock.getToughJetFlights();
 	}
 }
