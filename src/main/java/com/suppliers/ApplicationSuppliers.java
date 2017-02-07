241bf5c5182273855cc6f9336b00c931874107a5
Alexandre da Silva <alexandre.silva@wplex.com.br>
2017-02-07 12:07:37 -0200

    Clean code.

--- src/main/java/com/suppliers/ApplicationSuppliers.java
+++ src/main/java/com/suppliers/ApplicationSuppliers.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApplicationAPI
+public class ApplicationSuppliers
 {
     public static void main(final String[] args)
     {
-        SpringApplication.run(ApplicationAPI.class, args);
+        SpringApplication.run(ApplicationSuppliers.class, args);
     }
 }
